ea606237da86759072b37d2a12388b0087481211
h <ho.kim@gnu.ac.kr>
2020-10-19 15:32:48 +0900

    Add Database

---
+++ lib/models/db.dart
@@ -0,0 +1,109 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+import 'habit.dart';
+
+/// 데이터베이스에 접근할 수 있는 클래스입니다.
+class DB {
+  static String get _dbName => 'database.db';
+
+  /// 데이터베이스 테이블 정보
+  static Map<String, Map<String, String>> get _tables {
+    return {
+      Habit.tableName: Habit.types,
+    };
+  }
+
+  static DB _instance;
+
+  /// 데이터베이스 인스턴스를 가져옵니다.
+  /// 데이터베이스가 초기화되지 않았다면, 이를 초기화합니다.
+  static Future<DB> getInstance() async {
+    if (_instance == null) {
+      final db = await openDatabase(
+        // db 경로
+        join(await getDatabasesPath(), _dbName),
+        // db 생성자
+        onCreate: (db, version) async {
+          for (final entry in _tables.entries) {
+            await _createTable(db, entry.key, entry.value);
+          }
+        },
+        // db 버전
+        version: 1,
+      );
+      _instance = DB(db);
+    }
+    return _instance;
+  }
+
+  static Future<void> _createTable(
+    Database db,
+    String tableName,
+    Map<String, String> types,
+  ) async {
+    final params = types.entries
+        .map(
+          (entry) => '${entry.key} ${entry.value}',
+        )
+        .join(', ');
+    await db.execute(
+      'CREATE TABLE $tableName($params)',
+    );
+  }
+
+  /// 데이터베이스를 제거하고, 인스턴스를 초기화합니다.
+  Future<void> deleteAll() async {
+    await deleteDatabase(
+      join(await getDatabasesPath(), _dbName),
+    );
+    _instance = null;
+  }
+
+  final Database _db;
+
+  const DB(this._db);
+
+  /// SQLite Database 인스턴스를 가져옵니다.
+  Database get inner => _db;
+
+  /// 데이터베이스에 값을 추가합니다.
+  Future<void> insert(DBTable object) async {
+    await _db.insert(
+      object.getTableName,
+      object.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  /// 값을 갱신합니다.
+  Future<void> update(DBTable object) async {
+    await _db.update(
+      object.getTableName,
+      object.toMap(),
+      where: 'id = ?',
+      whereArgs: [object.id],
+    );
+  }
+}
+
+/// 데이터베이스 테이블을 구성하는 추상 클래스입니다.
+abstract class DBTable {
+  final int id;
+
+  const DBTable(this.id);
+
+  String get getTableName;
+  Map<String, dynamic> toMap();
+
+  /// 데이터베이스에 값을 추가합니다.
+  Future<void> insert() async => (await DB.getInstance()).insert(this);
+
+  /// 값을 갱신합니다.
+  Future<void> update() async => (await DB.getInstance()).update(this);
+
+  @override
+  String toString() {
+    return toMap().toString();
+  }
+}
--- lib/models/habit.dart
+++ lib/models/habit.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:longlive/models/db.dart';
 
 enum HabitType {
+  Unknown,
   Interest,
   FaceAndSkin,
   Disease,
@@ -21,8 +23,27 @@ extension HabitTypeExtension on HabitType {
         return '정신';
       case HabitType.Eating:
         return '영양/식습관';
+      case HabitType.Unknown:
       default:
         return '알 수 없음';
+    }
+  }
+
+  static HabitType fromDescription(String description) {
+    switch (description) {
+      case '흥미':
+        return HabitType.Interest;
+      case '얼굴/피부':
+        return HabitType.FaceAndSkin;
+      case '지병':
+        return HabitType.Disease;
+      case '정신':
+        return HabitType.Mental;
+      case '영양/식습관':
+        return HabitType.Eating;
+      case '알 수 없음':
+      default:
+        return HabitType.Unknown;
     }
   }
 
@@ -38,6 +59,7 @@ extension HabitTypeExtension on HabitType {
         return Colors.purple;
       case HabitType.Eating:
         return Colors.lightGreen;
+      case HabitType.Unknown:
       default:
         return Colors.white30;
     }
@@ -55,26 +77,44 @@ extension HabitTypeExtension on HabitType {
         return Colors.purpleAccent;
       case HabitType.Eating:
         return Colors.lightGreenAccent;
+      case HabitType.Unknown:
       default:
         return Colors.white;
     }
   }
 }
 
-class Habit {
+class Habit extends DBTable {
+  // --------------
+  //  Table Fields
+  // --------------
+
   final String name;
   final HabitType ty;
 
   bool enabled;
 
+  // -------------------
+  //  Table Constructor
+  // -------------------
+
   Habit({
+    int id,
     this.name,
     this.ty,
     this.enabled = false,
-  });
+  }) : super(id);
+
+  // ----------------
+  //  Object Methods
+  // ----------------
+
+  // -----------------------
+  //  Object Static Methods
+  // -----------------------
 
   static List<Habit> all() => [
-        Habit(ty: HabitType.Interest, name: '킥보드'),
+        Habit(ty: HabitType.Interest, name: '자전거/킥보드'),
         Habit(ty: HabitType.Interest, name: '오토바이'),
         Habit(ty: HabitType.FaceAndSkin, name: '탈모'),
         Habit(ty: HabitType.FaceAndSkin, name: '여드름'),
@@ -85,14 +125,56 @@ class Habit {
         Habit(ty: HabitType.Disease, name: '고혈압'),
         Habit(ty: HabitType.Disease, name: '당뇨'),
         Habit(ty: HabitType.Mental, name: '우울증'),
-        Habit(ty: HabitType.Mental, name: '아싸'),
-        Habit(ty: HabitType.Mental, name: '무교'),
         Habit(ty: HabitType.Eating, name: '야식'),
-        Habit(ty: HabitType.Eating, name: '3끼X'),
+        Habit(ty: HabitType.Eating, name: '불규칙적 식사'),
         Habit(ty: HabitType.Eating, name: '음주'),
         Habit(ty: HabitType.Eating, name: '흡연'),
         Habit(ty: HabitType.Eating, name: '배달음식'),
         Habit(ty: HabitType.Eating, name: '라면'),
         Habit(ty: HabitType.Eating, name: '인스턴트'),
       ];
+
+  //------------------------------------------------------------------------//
+  // ---------------
+  //  Table Methods
+  // ---------------
+
+  static String get tableName => 'habits';
+  static Map<String, String> get types {
+    return {
+      'id': 'INTEGER PRIMARY KEY AUTOINCREMENT',
+      'name': 'TEXT',
+      'ty': 'TEXT',
+    };
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'ty': ty.description,
+    };
+  }
+
+  static Habit fromMap(Map<String, dynamic> map) {
+    return Habit(
+      id: map['id'],
+      name: map['name'],
+      ty: HabitTypeExtension.fromDescription(map['ty']),
+    );
+  }
+
+  // ----------------------
+  //  Table Static Methods
+  // ----------------------
+
+  String get getTableName => tableName;
+
+  static Future<List<Habit>> getList() async {
+    final db = await DB.getInstance();
+    final List<Map<String, dynamic>> maps = await db.inner.query(tableName);
+    return List.generate(maps.length, (i) => Habit.fromMap(maps[i]));
+  }
+
+  //------------------------------------------------------------------------//
 }
